94a000e024a014a44da75e7ba2af25ed46e127ef
MohamedGoharFCIH <engmabdo85@gmail.com>
2022-08-01 14:51:10 +0200

    fix model user temprature field change to int

--- models/user.py
+++ models/user.py
@@ -1,5 +1,4 @@
-import mongoengine as db
-from .medicine import Medicine
+from app import db 
 import datetime
 
 
@@ -7,10 +6,13 @@ class User(db.Document):
     name = db.StringField(required=True)
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True)
-    temperature = db.FloatField()
+    temperature = db.IntField()
     lat = db.FloatField()
     lng = db.FloatField()
     age = db.IntField()
     created_at = db.DateField(default=datetime.datetime.utcnow)
     updated_at = db.DateField(default=datetime.datetime.utcnow)
-    medicines = db.ListField(db.ReferenceField(Medicine))
+    medicines = db.ListField(db.ReferenceField('Medicine'))
+    def __unicode__(self):
+        return self.name
+    
