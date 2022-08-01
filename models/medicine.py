7fd28d673bb101129b5f6bcfe9994cb346f40b90
MohamedGoharFCIH <engmabdo85@gmail.com>
2022-08-01 14:49:59 +0200

    add datetime feilds in medicine models

--- models/medicine.py
+++ models/medicine.py
@@ -1,5 +1,11 @@
-import mongoengine as db
+from app import db
+import datetime
 
 class Medicine(db.Document):
     name = db.StringField(required=True)
     description = db.StringField()
+    created_at = db.DateField(default=datetime.datetime.utcnow)
+    updated_at = db.DateField(default=datetime.datetime.utcnow)
+    
+    def __unicode__(self):
+        return self.name
